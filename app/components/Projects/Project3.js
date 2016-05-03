ff7b7805994578f6f9c00cd561386f90881d0172
Kylo Xue <[jiao_xue@hotmail.com]>
2016-05-03 14:39:55 -0400

    fix video

--- app/components/Projects/Project3.js
+++ app/components/Projects/Project3.js
@@ -1,16 +1,23 @@
 var React = require('react');
 import Project from "./Project";
+import YouTube from "../../../node_modules/react-youtube/dist/YouTube";
 
 var Project3 = React.createClass({
     render() {
+        const opts = {
+         height: 'auto',
+         width: 'auto',
+           autoplay: 0
+         }
+
         const ProjectName = "GameOn";
         const Tagline ="Re-Invent Sports";
 
         const Titles = [
             "Problem",
             "Market",
+            "Inclusive Experience",
             "Digital Interface",
-            "Physical Interface",
             "WireFrame",
             "Hi-Fidelity Mockup",
             "Future Development",
@@ -18,7 +25,7 @@ var Project3 = React.createClass({
 
         const Images =[
             "./media/img/gameon/1.png",
-            "./media/img/gameon/apple-touch-startup-image01.png",
+            "./media/img/gameon/mockup.png",
             "./media/img/gameon/ar-202.png",
             "./media/img/gameon/ar-303.png",
             "./media/img/gameon/game_mockup_mine07.png",
@@ -31,11 +38,11 @@ var Project3 = React.createClass({
 
             "Real Time Sports Fan Engagement - GameOn is an immersive companion app for hard-core fans and pleasure goers alike.",
             //problem
-            "Stadium fans loose out on the experience that stay home fans have Fans needs more than halftime shows to engage themselves at the venue and come backn In person experience does not match fans’ consumption habits",
+            "Stadium fans loose out on the experience that stay home fans have. Fans needs more than halftime shows to engage themselves at the venue and come back. In person experience does not match fans’ consumption habits",
             //solution
             "PWC research suggests that CAGR for the sports market to go from $56.9 billion to $70.7 billion just in North America. In this 8% of it is expected to be in sponsorship and gate revenues. The aim is to not only Lapsed Season Holders but also groups and Corporate tickets. Many sports fans we talked about are very interested in such a Technology and were our inspiration behind this. ",
 
-            "Hi-Fidelity Mockup",
+            "GameOn allows the onsite sports fans to engage with their team and game, giving them an unique experence and sense of unity. It encourages more fans to come in person to cheer up with their teams",
 
             "Future Development",
         ].map((info, i)=> <Project keys={i} info={info}/> );
@@ -56,6 +63,17 @@ var Project3 = React.createClass({
                     {Contents[2]}
                      {Titles[1]}
                     {Contents[3]}
+                    <YouTube
+                       videoId="5ugPo8z-5hM"
+                       opts={opts}
+                       onReady={this._onReady}
+                     />
+             </div>
+
+             <div className="project-page-img addborder" >
+                 {Titles[2]}
+                 {Contents[4]}
+                 <img src ={Images[1]} /><p><br></br></p>
              </div>
 
              <div className="project-page-img">
